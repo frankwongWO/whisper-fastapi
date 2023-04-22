693dcc581cfa0cc5d937c3dd1f960d5052a601c8
DESKTOP-GT6M64H\frank <frank.wong@digital-one-cloud.com>
2023-04-22 14:25:39 +0800

    no message

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import io
 import shutil
 import datetime
 from werkzeug.utils import secure_filename
@@ -7,6 +8,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from faster_whisper import WhisperModel
 from translate import Translator
 from pydantic import BaseModel
+import base64
+import time
 
 
 app = FastAPI()
@@ -32,13 +35,16 @@ async def transcribe(
     device: str = Form("cuda"),
     to_lang: str = Form(None),
     file: UploadFile = File(...),
+    beam_size: int = Form(5),
 ):
+    start_time = time.time()  # 记录开始时间
     print(
         f"content_type: {file.content_type}\n"
         f"to_lang: {to_lang}\n"
         f"model_size: {model_size}\n"
         f"compute_type: {compute_type}\n"
-        f"device: {device}"
+        f"device: {device}\n"
+        f"beam_size: {beam_size}\n"
     )
     # Check if the file is an audio file
     ALLOWED_FILE_TYPES = {
@@ -68,33 +74,22 @@ async def transcribe(
     with open(target_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # # Save the uploaded file to disk
-    # with open(file.filename, "wb") as buffer:
-    #     shutil.copyfileobj(file.file, buffer)
-
-    # model_size = tiny, small, medium, large-v2,
-    model_size = model_size
+    # model_size = tiny, small,base, medium, large-v2,
+    # compute_type= "int8", "int8_float16", "float16", "None"
 
     model = WhisperModel(model_size, device=device, compute_type=compute_type)
 
-    # Run on GPU with FP16
-    # model = WhisperModel(model_size, device="cuda", compute_type="float16")
-    # or run on GPU with INT8
-    # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
-    # or run on CPU with INT8
-    # model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
     # Transcribe the file using faster-whisper
-    segments, info = model.transcribe(target_path, beam_size=5)
+    segments, info = model.transcribe(target_path, beam_size=beam_size)
+
     print(
         "Detected language '%s' with probability %f"
         % (info.language, info.language_probability)
     )
 
+    translator = Translator(from_lang=info.language, to_lang=to_lang)
     # Translate the segments text
-
     if to_lang != None:
-        translator = Translator(from_lang=info.language, to_lang=to_lang)
         segments_list = [
             {
                 "start": segment.start,
@@ -114,11 +109,88 @@ async def transcribe(
 
     print(segments_list)
 
+    end_time = time.time()  # 记录结束时间
+    processing_time = end_time - start_time  # 计算处理时间
+
+    print("processing_time", processing_time)
+
     return {
         "text": text,
         "segments": segments_list,
         "language": info.language,
         "language_probability": info.language_probability,
+        "processing_time": processing_time,  # 返回处理时间
+    }
+
+
+class base64DataTranscribe_Request(BaseModel):
+    file: str
+    model_size: str
+    compute_type: str
+    device: str = "cuda"
+    to_lang: str = None
+    beam_size: int = 5
+
+
+@app.post("/base64DataTranscribe")
+async def base64DataTranscribe(req: base64DataTranscribe_Request):
+    start_time = time.time()  # 记录开始时间
+    print(
+        f"base64Str: {req.file[:50]}....\n",
+        f"to_lang: {req.to_lang}\n"
+        f"model_size: {req.model_size}\n"
+        f"compute_type: {req.compute_type}\n"
+        f"device: {req.device}",
+    )
+
+    data = base64.b64decode(req.file)
+    binary_data = io.BytesIO(data)
+
+    model = WhisperModel(
+        req.model_size, device=req.device, compute_type=req.compute_type
+    )
+
+    segments, info = model.transcribe(
+        binary_data,
+        beam_size=req.beam_size,
+    )
+
+    print(
+        "Detected language '%s' with probability %f"
+        % (info.language, info.language_probability)
+    )
+
+    translator = Translator(from_lang=info.language, to_lang=req.to_lang)
+    if req.to_lang != None:
+        segments_list = [
+            {
+                "start": segment.start,
+                "end": segment.end,
+                "text": translator.translate(segment.text),
+            }
+            for segment in segments
+        ]
+    else:
+        segments_list = [
+            {"start": segment.start, "end": segment.end, "text": segment.text}
+            for segment in segments
+        ]
+
+    text = ",".join([segment["text"] for segment in segments_list])
+    print(text)
+    print(segments_list)
+
+    end_time = time.time()  # 记录结束时间
+    processing_time = end_time - start_time  # 计算处理时间
+
+    print("processing_time", processing_time)
+
+    return {
+        "text": text,
+        "segments": segments_list,
+        "language": info.language,
+        "language_probability": info.language_probability,
+        "processing_time": processing_time,  # 返回处理时间
     }
 
 
